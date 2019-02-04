848ff22f30e5e51d3571ec1f432cceb58446adfd
Chloe <youde.shihou@gmail.com>
2019-02-04 15:33:23 -0500

    almost done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,19 +17,48 @@ namespace ConsoleApp1
             double heightInches;
             double totalHeightCM;
 
-            System.Console.Write("Hey, you with the teeth... What is your first name?" );
+            System.Console.Write("Hey, you with the teeth... What is your first name? ");
             firstName = System.Console.ReadLine();
 
-            System.Console.Write("I pity fools named " + firstName + "What is your middle inital?");
+            System.Console.Write("I pity fools named " + firstName + ". What is your middle inital? ");
             middleInitial = System.Console.ReadLine();
 
-            System.Console.Write("Quit yo jibber-jabber and tell me your last name!");
+            System.Console.Write("Quit yo jibber-jabber and tell me your last name! ");
             lastName = System.Console.ReadLine();
 
-            fullName = firstName + middleInitial + lastName;
+            fullName = firstName + " " + middleInitial + " " + lastName;
 
-            System.Console.Write("Ok, " + fullName + "I'm Mr. T and I'm a Night Elf Mohawk");
+            System.Console.Write("Ok, " + fullName + " I'm Mr. T and I'm a Night Elf Mohawk. ");
+            System.Console.Write("How many feet tall are you " + firstName + " ?");
+            heightFeet = int.Parse(System.Console.ReadLine());
 
+            System.Console.Write("Thanks fool. How many inches go with that? ");
+            heightInches = int.Parse(System.Console.ReadLine());
+
+            totalHeightCM = ((heightFeet * 12) + heightInches) * 2.54;
+
+            System.Console.Write(firstName + ", how old are you? ");
+            age = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Are you a citizen? TRUE OR FALSE FOOL ");
+            isCitizen = bool.Parse(System.Console.ReadLine());
+
+            if (age < 18)
+            {
+                canVote = false;
+            }
+            
+            if (age >= 18)
+                if (isCitizen = true)
+            {
+                    canVote = true;
+            }
+            else
+                {
+                    canVote = false;
+                }
+
+            System.Console.Write(fullName + " " + totalHeightCM + " " + canVote);
         }
     }
 }
